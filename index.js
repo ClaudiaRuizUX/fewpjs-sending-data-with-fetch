5b9b9b275cf1934aba07f75206231c9c8bf80877
Claudia <claudiaruizux@gmail.com>
2022-03-29 20:08:26 +1100

    practice json server

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+document.querySelector('button').addEventListener('click', () => {
+    submitData('Andre', 'test@test.com')
+})
+
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        eamil
+    })
+})
+    .then(res => res.json())
+    .then(json => {
+        document.body.innerHTML = json['id']
+    })
+    .catch((error) => {
+        document.body.innerHTML = error.message;
+    })
+}
